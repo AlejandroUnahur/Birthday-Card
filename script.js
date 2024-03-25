1a155d3a6b90d65c1e30a4cd3b7f1b8759cb9d00
Alejandro Unahur <hugoalejandro.vaca@estudiantes.unahur.edu.ar>
2024-03-25 01:01:55 -0300

    minor changes

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function playAudio() {
     audio.pause();
     audio.currentTime = 0; // Reset the audio to the beginning
   }
+
+  // Verificar si el navegador soporta el evento visibilitychange
+if (typeof document.hidden !== "undefined") {
+  // Escuchar el evento visibilitychange
+  document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+      // Si la página se oculta, detener el audio
+      stopAudio();
+    }
+  });
+}
